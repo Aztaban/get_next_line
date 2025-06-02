45d7bfe43a17e333561ef0d673abb0c9c12fe347
Martin Justa <mjusta@c2r7s7.42prague.com>
2025-06-02 17:46:42 +0200

    Add header and empty files for functions.

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 17:41:15 by mjusta            #+#    #+#             */
+/*   Updated: 2025/06/02 17:46:25 by mjusta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FET_NEXT_LINE_H
+# define FET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h> 
+
+char	*get_next_line(int fd);
+
+#endif
