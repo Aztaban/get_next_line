0ee694ec9b6e08367525d4577dc19c56cda74d3e
Aztaban <Aztabanee@gmail.com>
2025-06-02 23:32:16 +0200

    add comments with expected function flow

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 17:41:24 by mjusta            #+#    #+#             */
-/*   Updated: 2025/06/02 17:42:36 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/06/02 23:19:00 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 char	*get_next_line(int fd)
 {
-	
+	// create stash
+	// allocate memory and create buffer.
+	// read the fd and store value into buffer. 
+	// store the buffer value in static variable stash. (allocate memory for stash?)
+	// if the string in stash doesnt contain new line load buffer again and add buffer to stash.
+	// repeat until the stash contains atleast one new line. 
+	// after that free the buffer. 
+	// if the buffer return empty return NULL and return the last value.(without new line)
+	// extract line from the stash and return it.
+	// before returning the last line free the stash from memory.
 }
