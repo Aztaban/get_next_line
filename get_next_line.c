9dc1c3589217d3ba87e846ba5872087cb0e11f3c
Martin Justa <mjusta@c2r7s7.42prague.com>
2025-06-06 17:21:30 +0200

    modified strojin to free stash from memory before storing the new string.

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,29 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 17:41:24 by mjusta            #+#    #+#             */
-/*   Updated: 2025/06/06 16:58:26 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/06/06 17:20:18 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*ft_strjoin(char const *s1, char const *s2)
+static char	*ft_strjoin_free_stash(char *stash, char const *s2)
 {
 	char	*result;
 	size_t	len1;
 	size_t	len2;
 
-	if (s1 == NULL || s2 == NULL)
+	if (stash == NULL || s2 == NULL)
 		return (NULL);
-	len1 = ft_strlen(s1);
+	len1 = ft_strlen(stash);
 	len2 = ft_strlen(s2);
 	result = (char *)malloc(len1 + len2 + 1);
 	if (!result)
 		return (NULL);
-	ft_memcpy(result, s1, len1);
+	ft_memcpy(result, stash, len1);
 	ft_memcpy(result + len1, s2, len2);
 	result[len1 + len2] = '\0';
+	free(stash);
 	return (result);
 }
 
@@ -66,8 +67,8 @@ char	*get_next_line(int fd)
 			free(buffer);
 			return (NULL);
 		}
-		buffer[bytes_read];
-		ft_strjoin(stash, buffer); //free the previous stash before returning new one
+		buffer[bytes_read] = '\0';
+		stash = ft_strjoin_free_stash(stash, buffer); //free the previous stash before returning new one
 	}
 	// after that free the buffer.
 	free(buffer); 
@@ -82,7 +83,12 @@ char	*get_next_line(int fd)
 #include <stdio.h>
 int	main(void)
 {
-	char *test_stash = "Hello\n42\nWorld";
-	printf("%s World", extract_line(test_stash));
+	char *test_stash = ft_strdup("Hello\n42\nWorld");
+	char *str = " of Warcraft";
+	//printf("%s World", extract_line(test_stash));
+
+	test_stash = ft_strjoin_free_stash(test_stash, str);
+	printf("%s", test_stash);
+	free(test_stash);
 }
 */
